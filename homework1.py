2be52e8dbaf369c099ec43393c7c73097afcc532
syyyyye <74963033+kimuse@users.noreply.github.com>
2022-03-09 22:23:51 +0900

    Create homework1.py

---
+++ homework1.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+A = np.array([[2, 5], 
+              [1, 3],
+              [4, 1]])
+              
+B = np.array([[4, -1], 
+            [2, 3],
+            [6, 2]])
+c = np.array([[1,2,2],
+              [3,4,5]])
+
+
+print("A = ", A)
+print("B = ", B)
+print("C = ", C)
+
+#print(A+B)
+print("(a)", A+B)
+
+#print(B-A)
+print("(b)",B-A)
+
+#print(B*C)
+print("(c)",B*C)
+
+#print(C*A)
+ print("(d)",C*A)
