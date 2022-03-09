ecda101b1cc0ec0f6c13c889a286a9c7ead8e815
syyyyye <74963033+kimuse@users.noreply.github.com>
2022-03-09 22:09:36 +0900

    Create homework3.py

---
+++ homework3.py
@@ -0,0 +1,19 @@
+import numpy as np 
+
+A = np.array([[1,2,3],
+              [4,5,6],
+              [7,8,9]])
+v = np.array([[1],
+              [2],
+              [3]])
+
+print("A.shape =",A.shape)
+print("v.shape =",v.shape)
+
+w=np.array([1,2,3])
+print("w =",w)
+print("w.shape =",w.shape)
+
+B = np.array([[1,2,3],[4,5,6]])
+print("B = ",B)
+print("B.shape =", B.shape)
